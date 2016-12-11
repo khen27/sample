e3a6f98c6a6bb3640945cba461134858e2749a26
khen27 <karl@braqs.co>
2016-12-11 13:45:56 -0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+
+  bool aukury_is_awesome = false;
+  if(aukury_is_awesome){
+    cout << "This is very true \n";
+  }
+  
+  return 0;
+}
